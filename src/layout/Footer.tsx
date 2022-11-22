f006e1608f99e1d12137c70ae438d2b8901560c6
neilshah2000 <neilshahlimited@hotmail.com>
2022-11-22 14:49:19 +0000

    menu icons

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,14 +1,29 @@
 import styles from './Footer.module.css'
 import { Link } from 'react-router-dom'
+import allContactsIcon from './../assets/all_contacts.svg'
+import favouritesIcon from './../assets/favourites.svg'
+import profileIcon from './../assets/profile.svg'
+import recentIcon from './../assets/recent.svg'
+
+function MenuItem({ to, text, icon }: any) {
+    return (
+        <div className={styles.menuItem}>
+            <Link to={to}>
+                <img src={icon} width={40} />
+                <div>{text}</div>
+            </Link>
+        </div>
+    )
+}
 
 function Footer() {
     return (
         <>
             <div className={styles.footer}>
-                <Link to="/">All Contacts</Link>
-                <Link to="/">Last Active</Link>
-                <Link to="/">Favourites</Link>
-                <Link to="/profile">Profile</Link>
+                <MenuItem to="/" text="All Contacts" icon={allContactsIcon}></MenuItem>
+                <MenuItem to="/" text="Last Active" icon={recentIcon}></MenuItem>
+                <MenuItem to="/" text="Favourites" icon={favouritesIcon}></MenuItem>
+                <MenuItem to="/profile" text="Profile" icon={profileIcon}></MenuItem>
             </div>
         </>
     )
